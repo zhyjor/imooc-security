ca1c32dd1ce7aebad41ea4fd0d863dba9bf4fc0b
zhyjor <zhyjor@163.com>
2019-06-04 08:29:15 +0800

    init

---
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.imooc;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
